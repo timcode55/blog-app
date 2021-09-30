50a2cdf9c33d64da1e726faf3c11c15089409b0f
Tim Petrvalsky <tpet55@yahoo.com>
2021-09-30 16:40:23 -0700

    Add separate DB for the testing environment

---
+++ tests/note_api.test.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+
+const api = supertest(app);
+
+test('notes are returned as json', async () => {
+	await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/);
+});
+
+afterAll(() => {
+	mongoose.connection.close();
+});
+
+test('there are two notes', async () => {
+	const response = await api.get('/api/blogs');
+
+	expect(response.body).toHaveLength(1);
+});
+
+test('the first note is about HTTP methods', async () => {
+	const response = await api.get('/api/blogs');
+
+	expect(response.body[0].title).toBe('How to Beat the High Cost of Living');
+});
