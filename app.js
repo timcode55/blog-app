6e4c173dd8e199bdd9427e9baae29076afc5f3c9
Tim Petrvalsky <tpet55@yahoo.com>
2021-10-02 14:43:46 -0700

    Create a user database for authenticating users

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const http = require('http');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -3,6 +3,8 @@ const helper = require('./test_helper');
 const supertest = require('supertest');
 const app = require('../app');
 const api = supertest(app);
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 const Blog = require('../models/blog');
 const initialBlogs = [
 	{
@@ -127,16 +129,66 @@ test('if likes property is missing then 0', async () => {
 	expect(lastBlog.likes).toEqual(0);
 });
 
-test('blog is missing title and url with 400', async () => {
-	const newBlog = {
-		author: 'Master Instructor',
-		likes: 325
-	};
+// test('blog is missing title and url with 400', async () => {
+// 	const newBlog = {
+// 		author: 'Master Instructor',
+// 		likes: 325
+// 	};
 
-	if (!newBlog.title && !newBlog.url) {
-		const sendBlog = await api.post('/api/blogs').send(newBlog).expect(400);
-	}
+// 	if (!newBlog.title && !newBlog.url) {
+// 		const sendBlog = await api.post('/api/blogs').send(newBlog).expect(400);
+// 	}
 
-	const blogsAtEnd = await helper.blogsInDb();
-	const lastBlog = blogsAtEnd[blogsAtEnd.length - 1];
+// 	const blogsAtEnd = await helper.blogsInDb();
+// 	const lastBlog = blogsAtEnd[blogsAtEnd.length - 1];
+// });
+
+describe('when there is initially one user in db', () => {
+	beforeEach(async () => {
+		await User.deleteMany({});
+
+		const passwordHash = await bcrypt.hash('sekret', 10);
+		const user = new User({ username: 'root', passwordHash });
+
+		await user.save();
+	});
+
+	test('creation succeeds with a fresh username', async () => {
+		const usersAtStart = await helper.usersInDb();
+
+		const newUser = {
+			username: 'mluukkai',
+			name: 'Matti Luukkainen',
+			password: 'salainen'
+		};
+
+		await api.post('/api/users').send(newUser).expect(200).expect('Content-Type', /application\/json/);
+
+		const usersAtEnd = await helper.usersInDb();
+		expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+		const usernames = usersAtEnd.map((u) => u.username);
+		expect(usernames).toContain(newUser.username);
+	});
+
+	test('creation fails with proper statuscode and message if username already taken', async () => {
+		const usersAtStart = await helper.usersInDb();
+
+		const newUser = {
+			username: 'root',
+			name: 'Superuser',
+			password: 'salainen'
+		};
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+			.expect('Content-Type', /application\/json/);
+
+		expect(result.body.error).toContain('`username` to be unique');
+
+		const usersAtEnd = await helper.usersInDb();
+		expect(usersAtEnd).toHaveLength(usersAtStart.length);
+	});
 });
